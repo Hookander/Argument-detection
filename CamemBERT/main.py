aae04e75ef4f45cc80bc01f42872a1cd64c7e2d9
BalthazarM <balthazar.martin123@gmail.com>
2024-03-13 09:58:36 +0100

    Add CamemBERT model and utility functions

--- CamemBERT/main.py
+++ CamemBERT/main.py
@@ -1,5 +1,26 @@
+from pprint import pprint
+import functools
+
 import torch
-camembert = torch.hub.load('pytorch/fairseq', 'camembert')
-camembert.eval()  # disable dropout (or leave in train mode to finetune)
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import pytorch_lightning as pl
+from transformers import AutoModelForSequenceClassification, CamembertForMaskedLM, AutoTokenizer, AutoConfig
+from datasets import load_dataset
+from sklearn.metrics import confusion_matrix, f1_score
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from tqdm.notebook import tqdm
 
 
+
+camembert = AutoModelForSequenceClassification.from_pretrained('camembert-base')
+
+
+def take_first_embedding(embeddings, attention_mask=None):
+    return embeddings[:, 0]
+
+def average_embeddings(embeddings, attention_mask):
+    return (attention_mask[..., None] * embeddings).mean(1)
